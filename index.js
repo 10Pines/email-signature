9d57a6cca0133119235b0c9795eff5bd3f492c28
David Correa <davidjonatan.c@gmail.com>
2021-02-19 11:52:25 -0300

    Add 'Back' button when showing the signature

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const generator = document.querySelector('.generator');
 const fullName = generator.querySelector('input[name=fullName]');
 const title = generator.querySelector('input[name=title]');
-const generateButton = generator.querySelector('button');
+const generateButton = generator.querySelector('.generator button');
 
 const container = document.querySelector('.container');
-const signature = document.querySelector('.signature');
+const signature = container.querySelector('.signature');
+const backButton = container.querySelector('.container button');
 const fullNameField = container.querySelector('section h1');
 const titleField = container.querySelector('section h2');
 
@@ -23,4 +24,10 @@ generateButton.addEventListener('click', () => {
     selection.addRange(range);
     document.execCommand("Copy");
     copied.classList.remove('hidden');
-})
+});
+
+backButton.addEventListener('click', () => {
+  container.classList.add('hidden');    
+  generator.classList.remove('hidden');
+  copied.classList.add('hidden');
+});
